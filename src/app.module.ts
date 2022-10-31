7781630c05eb42007f448dc9a5716a8066ac9266
jangot <pulin.pavel@gmail.com>
2022-10-31 20:17:01 +0100

    create versions

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,22 +4,30 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ApplicationModule } from './application/application.module';
 import { EnvironmentModule } from './environment/environment.module';
+import { VersionModule } from './version/version.module';
+import { create as createDataSource } from './utils/factories/dataSource';
 
 @Module({
     imports: [
-        TypeOrmModule.forRoot({
-            type: 'postgres',
-            host: 'localhost',
-            port: 5432,
-            username: 'postgres',
-            password: '123',
-            database: 'io_versions',
-            entities: [],
-            synchronize: true,
-            autoLoadEntities: true,
+        TypeOrmModule.forRootAsync({
+            useFactory: () => {
+                return {
+                    type: 'postgres',
+                    host: 'localhost',
+                    port: 5432,
+                    username: 'postgres',
+                    password: '123',
+                    database: 'io_versions',
+                    entities: [],
+                    synchronize: true,
+                    autoLoadEntities: true,
+                }
+            },
+            dataSourceFactory: (options) => createDataSource(options).initialize(),
         }),
         ApplicationModule,
         EnvironmentModule,
+        VersionModule,
     ],
     controllers: [AppController],
     providers: [AppService],
