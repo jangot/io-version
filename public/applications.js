fdc663f082fa2138e4e8ef710cc8b47bd1768710
jangot <pulin.pavel@gmail.com>
2022-11-03 21:53:07 +0100

    create env

--- public/applications.js
+++ public/applications.js
@@ -35,3 +35,61 @@ $(() => {
         return false;
     });
 });
+
+
+$(() => {
+    const nameInput = $('#createEnv input[name="name"]');
+    const descriptionInput = $('#createEnv input[name="description"]');
+    const indexInput = $('#createEnv input[name="index"]');
+    const saveButton = $('#createEnv .j-save');
+    const errorBlock = $('#createEnv .error');
+
+    $(nameInput).add(descriptionInput).add(indexInput)
+        .keyup(disableSend)
+        .change(disableSend);
+
+    saveButton.click(() => {
+        errorBlock.hide();
+
+        fetch('/api/environment', {
+            method: 'POST',
+            mode: 'cors',
+            cache: 'no-cache',
+            credentials: 'same-origin',
+            headers: { 'Content-Type': 'application/json' },
+            redirect: 'follow',
+            referrerPolicy: 'no-referrer',
+            body: JSON.stringify(getData())
+        })
+        .then(() => {
+            // TODO check not success result
+            // Looks like fetch doesn't go to catch
+            location.reload();
+        })
+        .catch(() => {
+            errorBlock.show();
+        })
+
+        return false;
+    });
+
+    function disableSend() {
+        const data = getData();
+
+        const disabled = !data.name || !data.description || data.orderIndex < 1;
+
+        saveButton.prop('disabled', disabled);
+    }
+
+    function getData() {
+        const name = nameInput.val();
+        const description = descriptionInput.val();
+        const orderIndex = indexInput.val();
+
+        return {
+            name,
+            description,
+            orderIndex: Number(orderIndex)
+        }
+    }
+});
--- src/module/environment/environment.service.ts
+++ src/module/environment/environment.service.ts
@@ -36,6 +36,7 @@ export class EnvironmentService {
     }
 
     remove(id: number) {
+
         return `This action removes a #${id} environment`;
     }
 
